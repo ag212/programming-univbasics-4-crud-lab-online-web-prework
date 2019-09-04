436543ffa6e03bbfc28a5938481286b5050423ab
Abraham Galva <ag212@icloud.com>
2019-09-04 17:53:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  crayon_colors= ["red","blue","green","arrays!"]
+  crayon_colors[3]
 end
 
 def update_element_from_index(array, index_number, element)
